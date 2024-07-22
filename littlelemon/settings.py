f40bf401e9b6650fa1c352846665d708e8c389ff
Raul Martin <raulmartin1295@gmail.com>
2024-07-22 17:59:30 +0200

    autenticación +1

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'restaurant',
+    'djoser', 
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+
+#add the following line
+DJOSER={"USER_ID_FIELD":"username"}
