5db9eb6cffd81e492dfd7deea99d748c59807295
Raul Martin <raulmartin1295@gmail.com>
2024-07-22 11:00:39 +0200

    Creación de los modelos MysQL

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    no_of_guests = models.PositiveIntegerField()
+    booking_date = models.DateField()
+    
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveBigIntegerField()
+    
