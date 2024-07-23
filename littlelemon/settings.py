62c9605303b7e4a2fa2038d1ec124ab1896c713e
Raul Martin <raulmartin1295@gmail.com>
2024-07-23 10:35:40 +0200

    Proyecto final

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': '#201995Raul',
+        'PASSWORD': 'root@123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
